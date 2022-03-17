7c64c586d0b1d6f8286e7fedb871b490f328d55b
Mitchell Oller <oller.mitchell@gmail.com>
2022-03-17 08:46:08 -0700

    changed 'computerSelection' to 'computerPlay'

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let playerOptions = ['rock',
     'paper',
     'scissors']
-function computerSelection() {
+function computerPlay() {
     return playerOptions[Math.floor(Math.random() * playerOptions.length)];
 }
-console.log(computerSelection())
+console.log(computerPlay());
