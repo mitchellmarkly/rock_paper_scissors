aa725aaab1c3dd869f8bc21ea1e5ba6c0fb5deab
Mitchell Oller <oller.mitchell@gmail.com>
2022-03-19 14:56:53 -0700

    added if/else statements for game logic

--- script.js
+++ script.js
@@ -6,7 +6,27 @@ function computerPlay() {
 let playerSelection = prompt('Make your choice: Rock, Paper or Scissors');
 let computerSelection = computerPlay();
 
-
+if (computerSelection == 'rock' && playerSelection == 'scissors') {
+    console.log("computer chose " + computerSelection + " you LOSE")
+}
+else if (computerSelection == 'rock' && playerSelection == 'paper') {
+    console.log("computer chose " + computerSelection + " you WIN")
+}
+else if (computerSelection == 'paper' && playerSelection == 'rock') {
+    console.log("computer chose " + computerSelection + " you WIN")
+}
+else if (computerSelection == 'paper' && playerSelection == 'scissors') {
+    console.log("computer chose " + computerSelection + " you LOSE")
+}
+else if (computerSelection == 'scissors' && playerSelection == 'rock') {
+    console.log("computer chose " + computerSelection + " you WIN")
+}
+else if (computerSelection == 'paper' && playerSelection == 'scissors') {
+    console.log("computer chose " + computerSelection + " you LOSE")
+}
+else if (computerSelection == playerSelection) {
+    console.log("computer chose " + computerSelection + " TIE GAME")
+}
 
 
 
